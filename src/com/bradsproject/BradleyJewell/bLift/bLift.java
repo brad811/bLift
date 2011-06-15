704c6144ab1e1ca9860f10e77cf60d1dce10eb72
brad811 <brad811@gmail.com>
2011-06-15 12:23:07 -0500

    Please don't break...

--- src/com/bradsproject/BradleyJewell/bLift/bLift.java
+++ src/com/bradsproject/BradleyJewell/bLift/bLift.java
@@ -1,14 +1,13 @@
 package com.bradsproject.BradleyJewell.bLift;
 
 import org.bukkit.Server;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.bradsproject.BradleyJewell.bLift.bLiftRedstoneListener;
 
 /**
  * bLift for Bukkit
@@ -33,37 +32,6 @@ public class bLift extends JavaPlugin
 	
 	public void onDisable()
 	{
-		System.out.println("bSwarm has been disabled!");
+		System.out.println("bLift has been disabled!");
 	}
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel,
-			String[] args)
-	{
-		if(!(sender instanceof Player))
-			return false;
-		
-		Player player = (Player) sender;
-		String commandName = cmd.getName().toLowerCase();
-		
-		if(commandName.equals("bswarm"))
-		{
-			try
-			{
-				if(args.length > 0)
-				{
-					return true;
-				} else
-				{
-					player.sendMessage("Sending your swarm...");
-					return true;
-				}
-			} catch (Exception e)
-			{
-				e.printStackTrace();
-			}
-		}
-		return false;
-	}
-	
 }
